7b82cecd2d08d333a1577b1c928265e1fa07024b
Rich Nakasato <richnakasato@hotmail.com>
2018-02-19 23:00:23 -0500

    implemented insert index with half finished test

--- linked-list/linkedlist.py
+++ linked-list/linkedlist.py
@@ -2,9 +2,9 @@ class Node(object):
 
     """Docstring for Node. """
 
-    def __init__(self, val=None, next=None):
+    def __init__(self, element=None, next=None):
         """TODO: to be defined1. """
-        self._val = val
+        self._element = element
         self._next = next
 
 
@@ -12,7 +12,43 @@ class LinkedList(object):
 
     """Docstring for LinkedList. """
 
-    def __init__(self, val=None, next=None):
+    def __init__(self, element=None, next=None):
         """TODO: to be defined1. """
-        self._head = Node(val, next)
+        self._head = Node(element, next)
+
+    def __len__(self):
+        count = 0
+        traverse = self._head
+        while traverse:
+            traverse = traverse._next
+            count += 1
+        return count
+
+    def __str__(self):
+        output = list()
+        traverse = self._head
+        while traverse:
+            output.append(str(traverse._element))
+            traverse = traverse._next
+        return ' '.join(output)
+
+    def size(self):
+        return len(self)
+
+    def insert(self, idx=0, element=None):
+        if idx == 0:
+            new_head = Node(element, self._head)
+            self._head = new_head
+            return
+        count = 1
+        prev = self._head
+        while prev:
+            if count == idx:
+                to_insert = Node(element, prev._next)
+                prev._next = to_insert
+                return
+            prev = prev._next
+            count += 1
+        raise IndexError('invalid index')
+
 
--- linked-list/testlinkedlist.py
+++ linked-list/testlinkedlist.py
@@ -9,8 +9,20 @@ class TestLinkedList(unittest.TestCase):
     def test_init(self):
         x = LinkedList(1)
         self.assertNotEqual(x, None)
-        print(x._head._val)
+        self.assertEqual(len(x), 1)
 
+    def test_insert(self):
+        x = LinkedList(0)
+        print(str(x))
+        x.insert(0, 10)
+        x.insert(0, 20)
+        x.insert(0, 30)
+        x.insert(0, 40)
+        self.assertEqual(len(x), 5)
+        x.insert(1, 50)
+        print(str(x))
+        x.insert(6, 50)
+        print(str(x))
 
 if __name__ == "__main__":
     unittest.main()
